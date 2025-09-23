8ebf0aa761bee67b34e2f908faf5534831c9aa40
henders <148650724+henders1@users.noreply.github.com>
2025-09-23 23:40:38 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,17 @@
+// data.js
+const cardData = {
+  firstName: "Jan",
+  lastName: "Kowalski",
+  nationality: "Polska",
+  birthDate: "1990-01-01",
+  pesel: "90010112345",
+  documentValidUntil: "2030-01-01",
+  fatherName: "Adam",
+  motherName: "Ewa",
+  gender: "Mężczyzna",
+  placeOfBirth: "Warszawa",
+  countryOfBirth: "Polska",
+  permanentAddress: "ul. Przykładowa 1, Warszawa",
+  registrationDate: "2010-01-01",
+  imageUrl: "https://i.imgur.com/example.jpg"
+};
