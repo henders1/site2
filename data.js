28d44cae8d39f8bb36ab13fc878d66aad07dab44
henders <148650724+henders1@users.noreply.github.com>
2025-09-24 19:41:40 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,17 +1,34 @@
-// data.js
-const cardData = {
-  firstName: "Jan",
-  lastName: "Kowalski",
-  nationality: "Polska",
-  birthDate: "1990-01-01",
-  pesel: "90010112345",
-  documentValidUntil: "2030-01-01",
-  fatherName: "Adam",
-  motherName: "Ewa",
-  gender: "Mężczyzna",
-  placeOfBirth: "Warszawa",
-  countryOfBirth: "Polska",
-  permanentAddress: "ul. Przykładowa 1, Warszawa",
-  registrationDate: "2010-01-01",
-  imageUrl: "https://i.imgur.com/example.jpg"
+// SIMPLE DATA CONFIGURATION
+// Just edit the values below and upload - it works everywhere!
+
+window.cardData = {
+    // BASIC INFO - Edit these values
+    name: "Jan",
+    surname: "Kowalski",
+    birthday: "01.01.1990",
+    pesel: "90010112345",
+    nationality: "polskie",
+    sex: "Mężczyzna",
+
+    // DOCUMENT INFO
+    seriesAndNumber: "ABC123456",
+    givenDate: "15.06.2024",
+    expiryDate: "15.06.2034",
+
+    // FAMILY INFO
+    fathersName: "Piotr",
+    mothersName: "Anna",
+    familyName: "Kowalski",
+    fathersFamilyName: "Kowalski",
+    mothersFamilyName: "Nowak",
+
+    // ADDRESS INFO
+    adress: "ul. Przykładowa 123, 00-001 Warszawa",
+    birthPlace: "nirszawa",
+    countryOfBirth: "Polska",
+
+    // PHOTO (optional - put Imgur link here)
+    image: "https://i.imgur.com/example.jpg"
 };
+
+console.log("Card data loaded:", window.cardData);
